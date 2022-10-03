0fb46456550bcb9dfeb1fdcf91cfc1ec36a770b8
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-10-03 21:53:36 +0200

    correction for spotbug security problem

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -3,22 +3,46 @@ package com.parkit.parkingsystem.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.Properties;
 
 
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
+    private Properties properties;
+
+    public DataBaseConfig(){
+        loadProperties("src/main/resources/databaseConfig.properties");
+    }
+
+    protected void loadProperties(String resourcesPath) {
+        Path propertiesFile = Paths.get(resourcesPath);
+        properties = new Properties();
+        try (BufferedReader reader = Files.newBufferedReader(propertiesFile, StandardCharsets.UTF_8)) {
+            properties.load(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod", "root", "mySQL2022!");
+        return DriverManager.getConnection(properties.getProperty("databaseUrl"),
+                properties.getProperty("databaseLogin"),
+                properties.getProperty("databasePassword"));
     }
 
     public void closeConnection(Connection con) {
